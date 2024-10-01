d20865b0f3df7a5ca098b53050d1c437a2a8391b
sergio <est.sergioa.contre1@unimilitar.edu.co>
2024-10-01 15:36:27 -0500

    Add files via upload

---
+++ 2P/Main.java
@@ -0,0 +1,10 @@
+import Ejercicio.Metodos;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Metodos Metods = new Metodos();
+            Metods.setVisible(true);
+
+        }
+}
